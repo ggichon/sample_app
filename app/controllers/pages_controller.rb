0d8d64d8d31fdaaed44bb399e60b767fa257273f
guy <ggichon@gmail.com>
2010-08-18 00:04:42 +0300

    done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,14 @@
 class PagesController < ApplicationController
+  layout "application"
   def home
+    @title="Home"
   end
 
   def contact
+    @title="Contact"
   end
-
+  
+  def about
+    @title="About"
+  end
 end
